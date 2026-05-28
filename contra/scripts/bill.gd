bab1f14fd4dbc0e8a694d323b5e620c0919837f1
Daniel Hartmann <522758+daniel-hartmann@users.noreply.github.com>
2026-05-28 14:28:52 -0300

    Added jumping down and crouching

--- contra/scripts/bill.gd
+++ contra/scripts/bill.gd
@@ -1,34 +1,69 @@
 extends CharacterBody2D
 
-
 const SPEED = 60.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -240.0
+
+var is_jumping := false
 
 @onready var animated_sprite = $AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() / 2 * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	var direction := Input.get_axis("left", "right")
+
+	var is_crouching := (
+		Input.is_action_pressed("down")
+		and direction == 0
+		and is_on_floor()
+	)
+
+	# Horizontal movement
+	if is_crouching:
+		velocity.x = 0
+	else:
+		if direction:
+			velocity.x = direction * SPEED
+			animated_sprite.flip_h = direction < 0
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# Drop through platform
+	if is_crouching:
+		animated_sprite.play("crouch")
+
+		if Input.is_action_just_pressed("jump"):
+			set_collision_mask_value(1, false)
+			is_jumping = false
+			$DropTimer.start()
+
+	# Normal jump
+	elif Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		is_jumping = true
 		animated_sprite.play("jump")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	
-	
-	if direction:
-		velocity.x = direction * SPEED
-		if velocity.y == 0:
-			animated_sprite.play("run")
-		animated_sprite.flip_h = direction < 0
+	# Air animations
+	elif not is_on_floor():
+		if is_jumping:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
+
+	# Ground animations
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if velocity.y == 0:
+		is_jumping = false
+
+		if direction:
+			animated_sprite.play("run")
+		else:
 			animated_sprite.play("idle")
 
 	move_and_slide()
+
+
+func _on_drop_timer_timeout() -> void:
+	set_collision_mask_value(1, true)
