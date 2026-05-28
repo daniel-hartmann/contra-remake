0de7179afc79caab7583ed61af23433ff1539f61
Matheus Cassali <matheuscassali@gmail.com>
2026-05-28 00:15:20 -0300

    animacao de pulo

--- contra/scripts/bill.gd
+++ contra/scripts/bill.gd
@@ -12,17 +12,23 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animated_sprite.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
+	
+	
 	if direction:
 		velocity.x = direction * SPEED
-		animated_sprite.play("run")
+		if velocity.y == 0:
+			animated_sprite.play("run")
 		animated_sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.y == 0:
+			animated_sprite.play("idle")
 
 	move_and_slide()
