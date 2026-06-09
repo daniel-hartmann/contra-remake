c48578e53fc1ce2a4942bb8c6fe3d91aaba5bbae
Daniel Hartmann <522758+daniel-hartmann@users.noreply.github.com>
2026-06-09 20:45:13 -0300

    Show player lives in the hud and hud follows camera

--- contra/scripts/game.gd
+++ contra/scripts/game.gd
@@ -1,7 +1,27 @@
 extends Node
 
+@onready var hud := $HUD
+
 var player_lives = 3;
 
 
 func _ready() -> void:
-	pass
+	# Three lives is 2 symbols in the HUD.
+	var lives = hud.get_node("Lives").get_children()
+	for i in range(0, player_lives - 1):
+		lives[i].show()
+
+
+func get_camera_bounds() -> Rect2:
+	"""Code repeated on main.gd"""
+	var camera = get_viewport().get_camera_2d()
+	var center = camera.get_screen_center_position()
+	var viewport_size = camera.get_viewport_rect().size
+	var visible_size = viewport_size / camera.zoom
+	var top_left = center - (visible_size / 2.0)
+	return Rect2(top_left, visible_size)
+
+func _physics_process(delta: float) -> void:
+	# HUD should seem fixed related to the camera
+	var camera_bounds = get_camera_bounds()
+	hud.global_position.x = camera_bounds.position.x
