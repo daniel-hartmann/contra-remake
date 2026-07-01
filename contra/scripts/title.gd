e8e302638b6da2a499b972f6fc157729509b8a0c
Daniel Hartmann <522758+daniel-hartmann@users.noreply.github.com>
2026-06-30 22:29:12 -0300

    Start now works, but keeping game scene as default for now

--- contra/scripts/title.gd
+++ contra/scripts/title.gd
@@ -3,16 +3,24 @@ extends Control
 
 @onready var main_container := $MainContainer
 var is_ready := false
+var one_player_selected := true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _unhandled_key_input(event: InputEvent) -> void:
+	if not is_ready:
+		return
+
 	if event.is_action_pressed("select"):
 		var current_one_player_index = %OnePlayerCursor.z_index
 		%OnePlayerCursor.z_index = %TwoPlayerCursor.z_index
 		%TwoPlayerCursor.z_index = current_one_player_index
+		one_player_selected = !one_player_selected
+
+	if event.is_action_pressed("start"):
+		get_parent().change_screen(preload("res://scenes/game.tscn").instantiate())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
